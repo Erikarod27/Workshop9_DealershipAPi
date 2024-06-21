57dabb0f59517f4bdc18ea935216dea13f3010c7
Sekwang Chia <sk123rulz@gmail.com>
2024-06-21 14:03:00 -0400

    Merged main into my code

--- src/main/java/com/pluralsight/DealershipAPI/controllers/VehicleController.java
+++ src/main/java/com/pluralsight/DealershipAPI/controllers/VehicleController.java
@@ -3,15 +3,11 @@ package com.pluralsight.DealershipAPI.controllers;
 import com.pluralsight.DealershipAPI.models.Vehicle;
 import com.pluralsight.DealershipAPI.services.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 @RestController
 public class VehicleController {
